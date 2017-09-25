398bf5552d5b943ef8c93cf1d4c31413b7c9270c
Cahyo Adhi <cahyo.adhi97@gmail.com>
2017-09-26 00:41:19 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'lesonline',
+	'hostname' => 'ec2-54-225-88-191.compute-1.amazonaws.com',
+	'username' => 'cjjlsysfnyrvwa',
+	'password' => 'b385332ca1d29ffe202fa3915b04b9d9c65ca246f6bc24c37b3e7534c1ff734d',
+	'database' => 'd8ujoan20goo8f',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
